2dae439ee6ff3174abbe9d2df2615d6101077117
jan9392 <53424055+jan9392@users.noreply.github.com>
2021-03-15 09:09:16 +0800

    Update JDRouterPush.py

--- JDRouterPush.py
+++ JDRouterPush.py
@@ -124,9 +124,9 @@ def pointOperateRecordsShow(headers,mac):
 # 推送通知
 def sendNotification(SERVERPUSHKEY,text,desp):
     # server推送
-    server_push_url = "https://sc.ftqq.com/" + SERVERPUSHKEY + ".send"
+    server_push_url = "https://sctapi.ftqq.com/" + SERVERPUSHKEY + ".send"
     params = {
-        "text" : text,
+        "title" : text,
         "desp" : desp
     }
     res = requests.get(url=server_push_url, params=params)
